fe386a03a5ec4da12bcf19ef73d10ae06e0e43a4
Max Dickinson <maxsantistadickinson@gmail.com>
2020-05-12 08:09:15 -0300

    Adding build package.

---
+++ build/precache-manifest.2d6b124b77c4968de641811c7ea6ca97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f11ec6f4cb8545eb730afcae7d869ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eee686f22978e6624d0",
+    "url": "/static/css/2.46a6b073.chunk.css"
+  },
+  {
+    "revision": "6eee686f22978e6624d0",
+    "url": "/static/js/2.c6e59220.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6e59220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b1bedf5ae2623d0fa6",
+    "url": "/static/js/main.d12d75fe.chunk.js"
+  },
+  {
+    "revision": "5f0730460d61896db8ec",
+    "url": "/static/js/runtime-main.78929541.js"
+  },
+  {
+    "revision": "a29911502b3b2dd49e601a6f98ae3f14",
+    "url": "/static/media/background.a2991150.png"
+  }
+]);
